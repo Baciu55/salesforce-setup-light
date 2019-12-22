6649103d7aace5fafab772f8f0e4cab81f94a320
Baciu55 <cerealpl@wp.pl>
2019-12-22 20:24:12 +0100

    fetch data from api

--- background.js
+++ background.js
@@ -1,15 +1,25 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({color: '#3aa757'}, () => {
-        console.log('The color is green.');
-    });
+chrome.runtime.onMessage.addListener(onMessage);
 
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
+function onMessage(message) {
+    fetch('https://my-dev-test-dev-ed.my.salesforce.com/services/data/v47.0/query/?q=select+Id+from+Account',
+    {
+        method: 'GET',
+        headers: createHeaders()
+    })
+    .then(response => {
+        console.log(response.body);
+        if (!response) throw "XDDD";
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+    return true;
+}
+
+function createHeaders() {
+    return new Headers({
+        'Authorization': 'Bearer 00D3X000002NiWW!AR0AQBIb1H8wHDllBMa_CGag9woJKPPlmgLVyCXADYpfvxbbEuSiaSU9IHXFHZc2YuSn_hxrfxZckC24trP9MnauSaoW_ugz',
+        'Accept': 'application/json; charset=UTF-8'
     });
-});
+}
